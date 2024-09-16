77de36d9edcf8cddd6c7d26f8fddd672abd44ef4
haidari <esmailhaidari24@gmail..com>
2024-09-16 11:44:38 +0200

    first commit

---
+++ chrome-extension:/mooikfkahbdckldjjndioackbalphokd/assets/prompt.js
@@ -0,0 +1,7 @@
+https://ehsan-react-cv.surge.sh/static/media/DancingScript-VariableFont_wght.3e7e73f4e34e4945f363.ttf
+
+
+https://ehsan-react-cv.surge.sh/static/media/cv.bbbd26dc73a26cd26072.PNG
+
+https://ehsan-react-cv.surge.sh/static/media/cv_react.de9289cf5512f95bce92.PNG
+
